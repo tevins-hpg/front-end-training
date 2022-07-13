4500aa10c5b08c202e9944c406f4f468a74703e2
Tanner Evins <tanner.evins@healthtrustpg.com>
2022-07-13 09:58:20 -0500

    Install tailwind

--- src/App.vue
+++ src/App.vue
@@ -26,12 +26,3 @@ const currentView = computed(() => {
   <Header />
   <component v-if="currentView" :is="currentView" />
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-</style>
